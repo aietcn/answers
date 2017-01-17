5bc0a6bcb9574e4544ca3ea192d976c821e6b7d8
aiet <lovetcn@gmail.com>
2017-01-17 20:45:02 +0800

    add flatbuffers and protobuf large data test

--- src/test/java/self/data/format/FlatbuffersTest.java
+++ src/test/java/self/data/format/FlatbuffersTest.java
@@ -224,5 +224,186 @@ public class FlatbuffersTest {
     assertTrue(avgtime>75&&avgtime<85);
   }
 
+  private byte[] largeData(){
+    FlatBufferBuilder builder = new FlatBufferBuilder();
+    int xVector = SensorData.createXVector(builder, new double[]{0.0, 3.3, 6.6, 9.9, 12.12, 15.15, 18.18, 21.21, 24.24, 27.27, 30.30});
+    int yVector = SensorData.createYVector(builder, new double[]{1.1, 4.4, 7.7, 10.10, 13.13, 16.16, 19.19, 22.22, 25.25, 28.28, 31.31});
+    int zVector = SensorData.createZVector(builder, new double[]{2.2, 5.5, 8.8, 11.11, 14.14, 17.17, 20.20, 23.23, 26.26, 29.29, 32.32});
+    int tVector = SensorData.createTimestampVector(builder, new long[]{System.currentTimeMillis(), System.currentTimeMillis(), System.currentTimeMillis(),
+        System.currentTimeMillis(), System.currentTimeMillis(), System.currentTimeMillis(),
+        System.currentTimeMillis(), System.currentTimeMillis(), System.currentTimeMillis(),
+        System.currentTimeMillis(), System.currentTimeMillis()});
+    int dataStreamOffset = SensorData.createSensorData(builder, tVector, xVector, yVector, zVector, Sensor.ACCELERATION);
+    int listVector = FlatbuffersData.createListVector(builder, new int[]{dataStreamOffset});
+
+    int x2Vector = SensorData.createXVector(builder, new double[]{0.0, 3.3, 6.6, 9.9, 12.12, 15.15, 18.18, 21.21, 24.24, 27.27, 30.30});
+    int y2Vector = SensorData.createYVector(builder, new double[]{1.1, 4.4, 7.7, 10.10, 13.13, 16.16, 19.19, 22.22, 25.25, 28.28, 31.31});
+    int z2Vector = SensorData.createZVector(builder, new double[]{2.2, 5.5, 8.8, 11.11, 14.14, 17.17, 20.20, 23.23, 26.26, 29.29, 32.32});
+    int t2Vector = SensorData.createTimestampVector(builder, new long[]{System.currentTimeMillis(), System.currentTimeMillis(), System.currentTimeMillis(),
+        System.currentTimeMillis(), System.currentTimeMillis(), System.currentTimeMillis(),
+        System.currentTimeMillis(), System.currentTimeMillis(), System.currentTimeMillis(),
+        System.currentTimeMillis(), System.currentTimeMillis()});
+    int dataStreamOffset2 = SensorData.createSensorData(builder, t2Vector, x2Vector, y2Vector, z2Vector, Sensor.MAGNETISM);
+    int listVector2 = FlatbuffersData.createListVector(builder, new int[]{dataStreamOffset2});
+
+    int uid = builder.createString("guava");
+    FlatbuffersData.startFlatbuffersData(builder);
+    FlatbuffersData.addUid(builder, uid);
+    FlatbuffersData.addTimestamp(builder, System.currentTimeMillis());
+    FlatbuffersData.addType(builder, UPLOAD);
+    FlatbuffersData.addList(builder, listVector);
+    FlatbuffersData.addList(builder, listVector2);
+    int offset = FlatbuffersData.endFlatbuffersData(builder);
+    FlatbuffersData.finishFlatbuffersDataBuffer(builder, offset);
+    byte[] bytes = builder.sizedByteArray();
+    return bytes;
+  }
+
+  @Test
+  public void testSerializeSizeLargeData() throws Exception {
+    byte[] bytes = largeData();
+    System.out.println(bytes.length);
+    assertTrue(bytes.length == 920);
+  }
+
+  @Test
+  public void testSerializeTimeLargeData() throws Exception {
+    List<Double> avgs = new ArrayList<>(700);
+    for (int j = 0; j < 1000; j++) {
+      List<Long> times = new ArrayList<>(10000);
+      for (int i = 0; i < 10000; i++) {
+        long before = System.nanoTime();
+        FlatBufferBuilder builder = new FlatBufferBuilder();
+        int xVector = SensorData.createXVector(builder, new double[]{0.0, 3.3, 6.6, 9.9, 12.12, 15.15, 18.18, 21.21, 24.24, 27.27, 30.30});
+        int yVector = SensorData.createYVector(builder, new double[]{1.1, 4.4, 7.7, 10.10, 13.13, 16.16, 19.19, 22.22, 25.25, 28.28, 31.31});
+        int zVector = SensorData.createZVector(builder, new double[]{2.2, 5.5, 8.8, 11.11, 14.14, 17.17, 20.20, 23.23, 26.26, 29.29, 32.32});
+        int tVector = SensorData.createTimestampVector(builder, new long[]{System.currentTimeMillis(), System.currentTimeMillis(), System.currentTimeMillis(),
+            System.currentTimeMillis(), System.currentTimeMillis(), System.currentTimeMillis(),
+            System.currentTimeMillis(), System.currentTimeMillis(), System.currentTimeMillis(),
+            System.currentTimeMillis(), System.currentTimeMillis()});
+        int dataStreamOffset = SensorData.createSensorData(builder, tVector, xVector, yVector, zVector, Sensor.ACCELERATION);
+        int listVector = FlatbuffersData.createListVector(builder, new int[]{dataStreamOffset});
+
+        int x2Vector = SensorData.createXVector(builder, new double[]{0.0, 3.3, 6.6, 9.9, 12.12, 15.15, 18.18, 21.21, 24.24, 27.27, 30.30});
+        int y2Vector = SensorData.createYVector(builder, new double[]{1.1, 4.4, 7.7, 10.10, 13.13, 16.16, 19.19, 22.22, 25.25, 28.28, 31.31});
+        int z2Vector = SensorData.createZVector(builder, new double[]{2.2, 5.5, 8.8, 11.11, 14.14, 17.17, 20.20, 23.23, 26.26, 29.29, 32.32});
+        int t2Vector = SensorData.createTimestampVector(builder, new long[]{System.currentTimeMillis(), System.currentTimeMillis(), System.currentTimeMillis(),
+            System.currentTimeMillis(), System.currentTimeMillis(), System.currentTimeMillis(),
+            System.currentTimeMillis(), System.currentTimeMillis(), System.currentTimeMillis(),
+            System.currentTimeMillis(), System.currentTimeMillis()});
+        int dataStreamOffset2 = SensorData.createSensorData(builder, t2Vector, x2Vector, y2Vector, z2Vector, Sensor.MAGNETISM);
+        int listVector2 = FlatbuffersData.createListVector(builder, new int[]{dataStreamOffset2});
+
+        int uid = builder.createString("guava");
+        FlatbuffersData.startFlatbuffersData(builder);
+        FlatbuffersData.addUid(builder, uid);
+        FlatbuffersData.addTimestamp(builder, System.currentTimeMillis());
+        FlatbuffersData.addType(builder, UPLOAD);
+        FlatbuffersData.addList(builder, listVector);
+        FlatbuffersData.addList(builder, listVector2);
+        int offset = FlatbuffersData.endFlatbuffersData(builder);
+        FlatbuffersData.finishFlatbuffersDataBuffer(builder, offset);
+        byte[] bytes = builder.sizedByteArray();
+        times.add(System.nanoTime() - before);
+      }
+      double avg = times.stream().mapToLong(Long::longValue).average().getAsDouble();
+      if (j > 300) avgs.add(avg);
+    }
+    double avgtime = avgs.stream().mapToDouble(Double::doubleValue).average().getAsDouble();
+    System.out.println(avgtime);
+    assertTrue(avgtime > 2400 && avgtime < 2500); //2450
+  }
+
+  @Test
+  public void testSerializeTimePrebuiltLargeData() throws Exception {
+    /* pre-allocated buffers does make things better */
+    List<Double> avgs = new ArrayList<>(700);
+    for (int j = 0; j < 1000; j++) {
+      List<Long> times = new ArrayList<>(10000);
+      ByteBuffer buffer = ByteBuffer.allocate(1024);
+      for (int i = 0; i < 10000; i++) {
+        long before = System.nanoTime();
+        FlatBufferBuilder builder = new FlatBufferBuilder(buffer);
+        int xVector = SensorData.createXVector(builder, new double[]{0.0, 3.3, 6.6, 9.9, 12.12, 15.15, 18.18, 21.21, 24.24, 27.27, 30.30});
+        int yVector = SensorData.createYVector(builder, new double[]{1.1, 4.4, 7.7, 10.10, 13.13, 16.16, 19.19, 22.22, 25.25, 28.28, 31.31});
+        int zVector = SensorData.createZVector(builder, new double[]{2.2, 5.5, 8.8, 11.11, 14.14, 17.17, 20.20, 23.23, 26.26, 29.29, 32.32});
+        int tVector = SensorData.createTimestampVector(builder, new long[]{System.currentTimeMillis(), System.currentTimeMillis(), System.currentTimeMillis(),
+            System.currentTimeMillis(), System.currentTimeMillis(), System.currentTimeMillis(),
+            System.currentTimeMillis(), System.currentTimeMillis(), System.currentTimeMillis(),
+            System.currentTimeMillis(), System.currentTimeMillis()});
+        int dataStreamOffset = SensorData.createSensorData(builder, tVector, xVector, yVector, zVector, Sensor.ACCELERATION);
+        int listVector = FlatbuffersData.createListVector(builder, new int[]{dataStreamOffset});
+
+        int x2Vector = SensorData.createXVector(builder, new double[]{0.0, 3.3, 6.6, 9.9, 12.12, 15.15, 18.18, 21.21, 24.24, 27.27, 30.30});
+        int y2Vector = SensorData.createYVector(builder, new double[]{1.1, 4.4, 7.7, 10.10, 13.13, 16.16, 19.19, 22.22, 25.25, 28.28, 31.31});
+        int z2Vector = SensorData.createZVector(builder, new double[]{2.2, 5.5, 8.8, 11.11, 14.14, 17.17, 20.20, 23.23, 26.26, 29.29, 32.32});
+        int t2Vector = SensorData.createTimestampVector(builder, new long[]{System.currentTimeMillis(), System.currentTimeMillis(), System.currentTimeMillis(),
+            System.currentTimeMillis(), System.currentTimeMillis(), System.currentTimeMillis(),
+            System.currentTimeMillis(), System.currentTimeMillis(), System.currentTimeMillis(),
+            System.currentTimeMillis(), System.currentTimeMillis()});
+        int dataStreamOffset2 = SensorData.createSensorData(builder, t2Vector, x2Vector, y2Vector, z2Vector, Sensor.MAGNETISM);
+        int listVector2 = FlatbuffersData.createListVector(builder, new int[]{dataStreamOffset2});
+
+        int uid = builder.createString("guava");
+        FlatbuffersData.startFlatbuffersData(builder);
+        FlatbuffersData.addUid(builder, uid);
+        FlatbuffersData.addTimestamp(builder, System.currentTimeMillis());
+        FlatbuffersData.addType(builder, UPLOAD);
+        FlatbuffersData.addList(builder, listVector);
+        FlatbuffersData.addList(builder, listVector2);
+        int offset = FlatbuffersData.endFlatbuffersData(builder);
+        FlatbuffersData.finishFlatbuffersDataBuffer(builder, offset);
+        byte[] bytes = builder.sizedByteArray();
+        times.add(System.nanoTime() - before);
+      }
+      double avg = times.stream().mapToLong(Long::longValue).average().getAsDouble();
+      if (j > 300) avgs.add(avg);
+    }
+    double avgtime = avgs.stream().mapToDouble(Double::doubleValue).average().getAsDouble();
+    System.out.println(avgtime);
+    assertTrue(avgtime > 2250 && avgtime < 2400); //2320
+  }
+
+  @Test
+  public void testDeserializationGetTypeLargeData() throws Exception {
+    byte[] bytes = largeData();
+    List<Double> avgs = new ArrayList<>(700);
+    for (int j = 0; j < 1000; j++) {
+      List<Long> times = new ArrayList<>(10000);
+      for (int i = 0; i < 10000; i++) {
+        long before = System.nanoTime();
+        FlatbuffersData data = FlatbuffersData.getRootAsFlatbuffersData(ByteBuffer.wrap(bytes));
+        data.type();
+        times.add(System.nanoTime() - before);
+      }
+      double avg = times.stream().mapToLong(Long::longValue).average().getAsDouble();
+      if (j > 300) avgs.add(avg);
+    }
+    double avgtime = avgs.stream().mapToDouble(Double::doubleValue).average().getAsDouble();
+    System.out.println(avgtime);
+    assertTrue(avgtime>43&&avgtime<45); //44
+  }
+
+  @Test
+  public void testDeserializationGetListDataLargeData() throws Exception {
+    byte[] bytes = largeData();
+
+    List<Double> avgs = new ArrayList<>(700);
+    for (int j = 0; j < 1000; j++) {
+
+      List<Long> times = new ArrayList<>(10000);
+      for (int i = 0; i < 10000; i++) {
+        long before = System.nanoTime();
+        FlatbuffersData data = FlatbuffersData.getRootAsFlatbuffersData(ByteBuffer.wrap(bytes));
+        data.list(0).x(0);
+        times.add(System.nanoTime() - before);
+      }
+      double avg = times.stream().mapToLong(Long::longValue).average().getAsDouble();
+      if (j > 300) avgs.add(avg);
+    }
+    double avgtime = avgs.stream().mapToDouble(Double::doubleValue).average().getAsDouble();
+    System.out.println(avgtime);
+    assertTrue(avgtime>75&&avgtime<80); //76
+  }
 
 }
--- src/test/java/self/data/format/ProtobufTest.java
+++ src/test/java/self/data/format/ProtobufTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 import static self.data.format.protobuf.ProtobufDataOuterClass.ProtobufData.Sensor.ACCELERATION;
+import static self.data.format.protobuf.ProtobufDataOuterClass.ProtobufData.Sensor.MAGNETISM;
 import static self.data.format.protobuf.ProtobufDataOuterClass.ProtobufData.Type.UPLOAD;
 import static self.data.format.protobuf.ProtobufDataOuterClass.ProtobufData.newBuilder;
 
@@ -154,6 +155,143 @@ public class ProtobufTest {
     double avgtime = avgs.stream().mapToDouble(Double::doubleValue).average().getAsDouble();
     System.out.println(avgtime);
     assertTrue(avgtime>390&&avgtime<400);
-
   }
+
+  private ProtobufData largeData(){
+    Builder builder = newBuilder();
+    builder.setType(UPLOAD);
+    builder.setUid("guava");
+    builder.setTimestamp(System.currentTimeMillis());
+    SensorData.Builder accDataBuilder = SensorData.newBuilder();
+    accDataBuilder.setSensor(ACCELERATION);
+    accDataBuilder.addX(0.0).addY(1.1).addZ(2.2).addTimestamp(System.currentTimeMillis());
+    accDataBuilder.addX(3.3).addY(4.4).addZ(5.5).addTimestamp(System.currentTimeMillis());
+    accDataBuilder.addX(6.6).addY(7.7).addZ(8.8).addTimestamp(System.currentTimeMillis());
+    accDataBuilder.addX(9.9).addY(10.10).addZ(11.11).addTimestamp(System.currentTimeMillis());
+    accDataBuilder.addX(12.12).addY(13.13).addZ(14.14).addTimestamp(System.currentTimeMillis());
+    accDataBuilder.addX(15.15).addY(16.16).addZ(17.17).addTimestamp(System.currentTimeMillis());
+    accDataBuilder.addX(18.18).addY(19.19).addZ(20.20).addTimestamp(System.currentTimeMillis());
+    accDataBuilder.addX(21.21).addY(22.22).addZ(23.23).addTimestamp(System.currentTimeMillis());
+    accDataBuilder.addX(24.24).addY(25.25).addZ(26.26).addTimestamp(System.currentTimeMillis());
+    accDataBuilder.addX(27.27).addY(28.28).addZ(29.29).addTimestamp(System.currentTimeMillis());
+    accDataBuilder.addX(30.30).addY(31.31).addZ(32.32).addTimestamp(System.currentTimeMillis());
+    SensorData.Builder magDataBuilder = SensorData.newBuilder();
+    magDataBuilder.setSensor(MAGNETISM);
+    magDataBuilder.addX(0.0).addY(1.1).addZ(2.2).addTimestamp(System.currentTimeMillis());
+    magDataBuilder.addX(3.3).addY(4.4).addZ(5.5).addTimestamp(System.currentTimeMillis());
+    magDataBuilder.addX(6.6).addY(7.7).addZ(8.8).addTimestamp(System.currentTimeMillis());
+    magDataBuilder.addX(9.9).addY(10.10).addZ(11.11).addTimestamp(System.currentTimeMillis());
+    magDataBuilder.addX(12.12).addY(13.13).addZ(14.14).addTimestamp(System.currentTimeMillis());
+    magDataBuilder.addX(15.15).addY(16.16).addZ(17.17).addTimestamp(System.currentTimeMillis());
+    magDataBuilder.addX(18.18).addY(19.19).addZ(20.20).addTimestamp(System.currentTimeMillis());
+    magDataBuilder.addX(21.21).addY(22.22).addZ(23.23).addTimestamp(System.currentTimeMillis());
+    magDataBuilder.addX(24.24).addY(25.25).addZ(26.26).addTimestamp(System.currentTimeMillis());
+    magDataBuilder.addX(27.27).addY(28.28).addZ(29.29).addTimestamp(System.currentTimeMillis());
+    magDataBuilder.addX(30.30).addY(31.31).addZ(32.32).addTimestamp(System.currentTimeMillis());
+    builder.addList(magDataBuilder.build()).addList(accDataBuilder.build());
+    return builder.build();
+  }
+
+
+  @Test
+  public void testSerializeSizeLargeData() throws Exception {
+    ProtobufData data = largeData();
+    byte[] bytes = data.toByteArray();
+    System.out.println(bytes.length);
+    assertTrue(bytes.length == 774);
+  }
+
+  @Test
+  public void testSerializeTimeLargeData() throws Exception {
+    List<Double> avgs = new ArrayList<>(700);
+    for (int j = 0; j < 1000; j++) {
+      List<Long> times = new ArrayList<>(10000);
+      for (int i = 0; i < 10000; i++) {
+        long before = System.nanoTime();
+        Builder builder = newBuilder();
+        builder.setType(UPLOAD);
+        builder.setUid("guava");
+        builder.setTimestamp(System.currentTimeMillis());
+        SensorData.Builder accDataBuilder = SensorData.newBuilder();
+        accDataBuilder.setSensor(ACCELERATION);
+        accDataBuilder.addX(0.0).addY(1.1).addZ(2.2).addTimestamp(System.currentTimeMillis());
+        accDataBuilder.addX(3.3).addY(4.4).addZ(5.5).addTimestamp(System.currentTimeMillis());
+        accDataBuilder.addX(6.6).addY(7.7).addZ(8.8).addTimestamp(System.currentTimeMillis());
+        accDataBuilder.addX(9.9).addY(10.10).addZ(11.11).addTimestamp(System.currentTimeMillis());
+        accDataBuilder.addX(12.12).addY(13.13).addZ(14.14).addTimestamp(System.currentTimeMillis());
+        accDataBuilder.addX(15.15).addY(16.16).addZ(17.17).addTimestamp(System.currentTimeMillis());
+        accDataBuilder.addX(18.18).addY(19.19).addZ(20.20).addTimestamp(System.currentTimeMillis());
+        accDataBuilder.addX(21.21).addY(22.22).addZ(23.23).addTimestamp(System.currentTimeMillis());
+        accDataBuilder.addX(24.24).addY(25.25).addZ(26.26).addTimestamp(System.currentTimeMillis());
+        accDataBuilder.addX(27.27).addY(28.28).addZ(29.29).addTimestamp(System.currentTimeMillis());
+        accDataBuilder.addX(30.30).addY(31.31).addZ(32.32).addTimestamp(System.currentTimeMillis());
+        SensorData.Builder magDataBuilder = SensorData.newBuilder();
+        magDataBuilder.setSensor(MAGNETISM);
+        magDataBuilder.addX(0.0).addY(1.1).addZ(2.2).addTimestamp(System.currentTimeMillis());
+        magDataBuilder.addX(3.3).addY(4.4).addZ(5.5).addTimestamp(System.currentTimeMillis());
+        magDataBuilder.addX(6.6).addY(7.7).addZ(8.8).addTimestamp(System.currentTimeMillis());
+        magDataBuilder.addX(9.9).addY(10.10).addZ(11.11).addTimestamp(System.currentTimeMillis());
+        magDataBuilder.addX(12.12).addY(13.13).addZ(14.14).addTimestamp(System.currentTimeMillis());
+        magDataBuilder.addX(15.15).addY(16.16).addZ(17.17).addTimestamp(System.currentTimeMillis());
+        magDataBuilder.addX(18.18).addY(19.19).addZ(20.20).addTimestamp(System.currentTimeMillis());
+        magDataBuilder.addX(21.21).addY(22.22).addZ(23.23).addTimestamp(System.currentTimeMillis());
+        magDataBuilder.addX(24.24).addY(25.25).addZ(26.26).addTimestamp(System.currentTimeMillis());
+        magDataBuilder.addX(27.27).addY(28.28).addZ(29.29).addTimestamp(System.currentTimeMillis());
+        magDataBuilder.addX(30.30).addY(31.31).addZ(32.32).addTimestamp(System.currentTimeMillis());
+        builder.addList(magDataBuilder.build()).addList(accDataBuilder.build());
+        ProtobufData data = builder.build();
+        byte[] bytes = data.toByteArray();
+        times.add(System.nanoTime() - before);
+      }
+      double avg = times.stream().mapToLong(Long::longValue).average().getAsDouble();
+      if (j > 300) avgs.add(avg);
+    }
+    double avgtime = avgs.stream().mapToDouble(Double::doubleValue).average().getAsDouble();
+    System.out.println(avgtime);
+    assertTrue(avgtime > 3600 && avgtime < 3800); //3750
+  }
+
+  @Test
+  public void testDeserializationGetTypeLargeData() throws Exception {
+    ProtobufData data = largeData();
+    byte[] bytes = data.toByteArray();
+
+    List<Double> avgs = new ArrayList<>(700);
+    for (int j = 0; j < 1000; j++) {
+      List<Long> times = new ArrayList<>(10000);
+      for (int i = 0; i < 10000; i++) {
+        long before = System.nanoTime();
+        ProtobufData protobufData = ProtobufData.parseFrom(bytes);
+        protobufData.getType();
+        times.add(System.nanoTime() - before);
+      }
+      double avg = times.stream().mapToLong(Long::longValue).average().getAsDouble();
+      if (j > 300) avgs.add(avg);
+    }
+    double avgtime = avgs.stream().mapToDouble(Double::doubleValue).average().getAsDouble();
+    System.out.println(avgtime);
+    assertTrue(avgtime>2050&&avgtime<2200); //2100
+  }
+
+  @Test
+  public void testDeserializationGetListDataLargeData() throws Exception {
+    ProtobufData data = largeData();
+    byte[] bytes = data.toByteArray();
+    List<Double> avgs = new ArrayList<>(700);
+    for (int j = 0; j < 1000; j++) {
+      List<Long> times = new ArrayList<>(10000);
+      for (int i = 0; i < 10000; i++) {
+        long before = System.nanoTime();
+        ProtobufData protobufData = ProtobufData.parseFrom(bytes);
+        protobufData.getList(0).getX(0);
+        times.add(System.nanoTime() - before);
+      }
+      double avg = times.stream().mapToLong(Long::longValue).average().getAsDouble();
+      if (j > 300) avgs.add(avg);
+    }
+    double avgtime = avgs.stream().mapToDouble(Double::doubleValue).average().getAsDouble();
+    System.out.println(avgtime);
+    assertTrue(avgtime>2050&&avgtime<2200); //2120
+  }
+
 }
